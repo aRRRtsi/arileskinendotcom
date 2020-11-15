5b3bec7cdecf3be367c9ed86b9a879d532958ba7
Ari Leskinen <arrrtsi@gmail.com>
2020-11-15 21:20:20 +0200

    Updating project depencies
    Did one example page for testing and installed and switched to sass

---
+++ pages/example/index.js
@@ -0,0 +1,13 @@
+import s from './Example.module.scss';
+
+export default function Example() {
+  return (
+    <div>
+      <main className={s.container}>
+        <h1>
+          Welcome to <a href='https://nextjs.org'>Example.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
